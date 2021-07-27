5da2c3f66c3db68a6789e0e5f3537a6e76af2456
darazuiscaptain <smallcaptain@proton.me>
2021-07-27 22:49:28 +0200

    Added api calls

---
+++ restaurant-app/src/api/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const BASE_URL = "http://localhost:5000/api/"
+
+export const ENDPOINTS = {
+    CUSTOMER: 'Customer'
+}
+
+export const createApiEndpoint = endpoint => {
+    let url = BASE_URL + endpoint + '/';
+    return {
+        fetchAll : () => axios.get(url),
+        fetchById : id => axios.get(url + id),
+        create : newRecord => axios.post(url, newRecord),
+        update : (id, updatedRecord) => axios.put(url + id, updatedRecord),
+        delete : id => axios.delete(url + id)
+    }
+}
