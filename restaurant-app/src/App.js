b6da82c1015e77405903751513745d3435c8a1bb
darazuiscaptain <smallcaptain@proton.me>
2021-07-27 22:51:09 +0200

    Removed default react text

--- restaurant-app/src/App.js
+++ restaurant-app/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Order from "./components/order/index"
+import { Container, Typography } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <Typography 
+      gutterBottom
+      variant="h2"
+      align="center">
+        Restaurant App
+      </Typography>
+      <Order />
+    </Container>
   );
 }
 
