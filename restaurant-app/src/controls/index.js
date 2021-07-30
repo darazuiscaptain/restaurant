b224ffc9417761e33dc20e7bd41e47994a637c88
darazuiscaptain <smallcaptain@proton.me>
2021-07-30 16:21:29 +0200

    Implemented controls folder

--- restaurant-app/src/controls/index.js
+++ restaurant-app/src/controls/index.js
@@ -1,3 +1,3 @@
-export {default as Input} from './Input'
-export {default as Select} from './Select'
-export {default as Button} from './Button'
+export { default as Button } from './Button'
+export { default as Input } from './Input'
+export { default as Select } from './Select'
