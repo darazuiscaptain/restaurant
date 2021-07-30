0ef12c2e0f761a06da09c7074284f630e10faf73
darazuiscaptain <smallcaptain@proton.me>
2021-07-30 16:19:35 +0200

    Implemented a pop up list for all the finished orders

--- restaurant-app/src/api/index.js
+++ restaurant-app/src/api/index.js
@@ -1,18 +1,19 @@
-import axios from 'axios'
+import axios from "axios"
 
-const BASE_URL = "http://localhost:5000/api/"
+const BASE_URL = 'http://localhost:5000/api/'
 
-export const ENDPOINTS = {
-    CUSTOMER: 'Customer'
+export const ENDPIONTS = {
+    CUSTOMER: 'Customer',
+    FOODITEM: 'FoodItem',
+    ORDER: 'Order'
 }
-
-export const createApiEndpoint = endpoint => {
+export const createAPIEndpoint = endpoint => {
     let url = BASE_URL + endpoint + '/';
     return {
-        fetchAll : () => axios.get(url),
-        fetchById : id => axios.get(url + id),
-        create : newRecord => axios.post(url, newRecord),
-        update : (id, updatedRecord) => axios.put(url + id, updatedRecord),
-        delete : id => axios.delete(url + id)
+        fetchAll: () => axios.get(url),
+        fetchById: id => axios.get(url + id),
+        create: newRecord => axios.post(url, newRecord),
+        update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
+        delete: id => axios.delete(url + id)
     }
 }
